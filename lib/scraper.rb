7ed28b7c8cdc4ae54114df63f078633aee11b58e
Oscar Vuong <oscarvuong@gmail.com>
2018-12-15 22:30:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,9 +8,12 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  
+
   end
 
+  def get_courses
+    
+  end
 
 
 
